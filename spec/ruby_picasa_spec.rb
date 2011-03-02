92ff113d4d962ce1945033cd85ad5c60e58da087
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-03-01 22:22:38 -0800

    Got specs working in rspec 2.

--- spec/ruby_picasa_spec.rb
+++ spec/ruby_picasa_spec.rb
@@ -333,11 +333,10 @@ describe Picasa do
       it('photo photo') { to_create RubyPicasa::Search, @photo, @photo }
     end
     
-    # I broke this test, though I'm not sure how to fix it (or why it ever 
-    # worked to begin with).  Shouldn't it always break when you pass in a 
-    # symbol instead of actual xml?  -- kueda 2009-12-29
     it 'should raise an error for invalid feed category types' do
-      @p.expects(:xml_data).with(:xml).returns([:xml, @album, @user])
+      @p.expects(:xml_data).with(:xml).returns([:xml, 
+        'http://schemas.google.com/photos/2007#foo', 
+        'http://schemas.google.com/photos/2007#bar'])
       lambda do
         @p.class_from_xml(:xml)
       end.should raise_error(RubyPicasa::PicasaError)
